d2ba6242ba13330918d99f4579f6aa690c46d6ab
JOSIAHTHEPROGRAMMER <josiahalexis15@gmail.com>
2026-02-22 18:59:27 -0400

    feat: define core database and table abstractions

---
+++ core/database.py
@@ -0,0 +1,9 @@
+class Database:
+    def __init__(self):
+        self.tables = {}
+
+    def create_table(self, name, columns):
+        pass
+
+    def get_table(self, name):
+        pass
---
+++ core/table.py
@@ -0,0 +1,10 @@
+class Table:
+    def __init__(self, name, columns):
+        self.name = name
+        self.columns = columns
+
+    def insert(self, row):
+        pass
+
+    def select_all(self):
+        pass
